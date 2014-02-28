0b0441a0e28d54c9c4da9d9d4de3bd66fa0a8486
Ben Hamill <git-commits@benhamill.com>
2014-02-28 17:36:55 -0600

    Factor out reused code.

--- lib/assembler/initializer.rb
+++ lib/assembler/initializer.rb
@@ -13,24 +13,29 @@ module Assembler
       missing_required_params = []
 
       self.class.required_params.each do |param_name|
-        instance_variable_set(
-          :"@#{param_name}",
-          full_options.fetch(param_name) do
-            missing_required_params << param_name
-          end
-        )
+        remember_value_or(full_options, param_name) do
+          missing_required_params << param_name
+        end
       end
 
       raise(ArgumentError, "missing keywords: #{missing_required_params.join(', ')}") if missing_required_params.any?
 
       self.class.optional_params.each do |param_name, default_value|
-        instance_variable_set(
-          :"@#{param_name}",
-          full_options.fetch(param_name) do
-            default_value
-          end
-        )
+        remember_value_or(full_options, param_name) do
+          default_value
+        end
       end
+    end
+
+    private
+
+    def remember_value_or(params, param_name, &block)
+      instance_variable_set(
+        :"@#{param_name}",
+        params.fetch(param_name) do
+          block.call
+        end
+      )
     end
   end
 end
