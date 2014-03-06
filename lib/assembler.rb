dc697a29e4368e94a949177771b68460f425c55e
Ben Hamill <git-commits@benhamill.com>
2014-03-06 01:04:23 -0600

    Add to param memoizing attributes.

--- lib/assembler.rb
+++ lib/assembler.rb
@@ -2,15 +2,15 @@ require "assembler/version"
 require "assembler/initializer"
 
 module Assembler
-  attr_reader :required_params, :optional_params, :all_param_names
+  attr_reader :all_param_names
 
   def assemble_from(*required, **optional)
     include Assembler::Initializer
 
-    @required_params = required
-    @optional_params = optional
-    @all_param_names = (required + optional.keys).map(&:to_sym)
+    required_params.push(*required)
+    optional_params.merge!(optional)
 
+    @all_param_names = (required_params + optional_params.keys).map(&:to_sym)
     attr_reader *all_param_names
     private *all_param_names
   end
@@ -20,5 +20,13 @@ module Assembler
     caller_file, caller_line, _ = caller.first.split(':')
     warn "The `assembler_initializer` method is deprecated and will be phased out in version 2.0. Please use `assemble_from` instead. Called from #{caller_file}:#{caller_line}."
     assemble_from(*args)
+  end
+
+  def required_params
+    @required_params ||= []
+  end
+
+  def optional_params
+    @optional_params ||= {}
   end
 end
